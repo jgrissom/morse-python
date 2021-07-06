39c59c4f2d243939d482f1de1cd04c6febd380e7
jgrissom <jgrissom91@gmail.com>
2021-07-06 16:20:53 -0500

    dot

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import uasyncio as asyncio
+from machine import Pin
+from async_switch import Switch
 
 def dot():
-    pass
+    print('.')
     
 def dash():
     pass
 
 async def main():
+    btn_red.close_func(dot)
     while True:
         await asyncio.sleep(.01)
 
 if __name__ == "__main__":
     try:
+        btn_red = Switch( Pin(18, Pin.IN, Pin.PULL_UP) )
         asyncio.run(main())
     finally:
         print('goodbye')
