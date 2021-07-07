704d53ae1d7cf77fb1a851d0cd75d3eb4489cbb3
jgrissom <jgrissom91@gmail.com>
2021-07-06 19:06:59 -0500

    save dots & dashes to list

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ import uasyncio as asyncio
 from machine import Pin
 from async_switch import Switch
 from internal import TinyPICODotStar
+from output import Output
 
 def dot():
-    print('.')
+    morse.append('.')
+    print(morse)
     t1 = asyncio.create_task(dotstar.async_flicker((191,64,191,.5), .1))
     
 def dash():
-    print('-')
+    morse.append('-')
+    print(morse)
     t1 = asyncio.create_task(dotstar.async_flicker((191,64,191,.5), .2))
 
 async def main():
@@ -23,6 +26,7 @@ if __name__ == "__main__":
         btn_green = Switch( Pin(5, Pin.IN, Pin.PULL_UP) )
         dotstar = TinyPICODotStar()
         dotstar.off()
+        morse = []
         asyncio.run(main())
     finally:
         print('goodbye')
